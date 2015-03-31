b794743e516690912eefc93a78775ffabe92e49b
egl74 <laptenauck@gmail.com>
2015-03-31 17:41:04 +0300

    head start

---
+++ DictionaryServer/MessageEnhacer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageNamespace;
+using Newtonsoft.Json;
+
+namespace DictionaryServer
+{
+    internal static class MessageEnhacer
+    {
+        private static Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        private static readonly string path = Environment.CurrentDirectory + "\\dictionary.json";
+
+        internal static Message Enhance(Message message)
+        {
+            switch (message.Action)
+            {
+                case ActionType.Add:
+                    return Add(message);
+                case ActionType.Remove:
+                    return Remove(message);
+                case ActionType.Edit:
+                    return Edit(message);
+            }
+            return null;
+        }
+
+        private static Message Add(Message message)
+        {
+            try
+            {
+                DictionaryFromJson();
+                dictionary.Add(message.Key, message.Value);
+                DictionaryToJson();
+                Console.WriteLine(DateTime.Now + " - Successfully added new entry '{0}'", message.Key);
+                return new Message { Result = "Successfully added new entry" + message.Key };
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(DateTime.Now + " - Attempt of adding an entry that already exists '{0}'", message.Key);
+                return new Message { Result = e.Message + " " + message.Key };
+            }
+        }
+
+        private static Message Remove(Message message)
+        {
+            try
+            {
+                DictionaryFromJson();
+                dictionary.Remove(message.Key);
+                DictionaryToJson();
+                Console.WriteLine(DateTime.Now + " - Successfully removed entry '{0}'", message.Key);
+                return new Message {Result = "Successfully removed entry" + message.Key};
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(DateTime.Now + " - Attempt of removing an entry that doesn't exist '{0}'", message.Key);
+                return new Message { Result = e.Message + " " + message.Key };
+            }
+        }
+
+        private static Message Edit(Message message)
+        {
+            DictionaryFromJson();
+            dictionary[message.Key] = message.Value;
+            DictionaryToJson();
+            Console.WriteLine(DateTime.Now + " - Successfully edited entry '{0}'", message.Key);
+            return new Message {Result = "Successfully edited entry" + message.Key};
+        }
+
+        private static void DictionaryFromJson()
+        {
+            var json = File.ReadAllText(path);
+            dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        }
+
+        private static void DictionaryToJson()
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(dictionary));
+        }
+
+        private static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+    }
+}
---
+++ DictionaryServer/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using MessageNamespace;
+
+namespace DictionaryServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //MessageEnhacer.Add(new Message{Key="Антон", Value = "гандон"});
+                MessageEnhacer.Enhance(new Message{Key = "Е", Action = ActionType.Remove});
+                TcpListener server = null;
+                try
+                {
+                    // Set the TcpListener on port 13000.
+                    Int32 port = 13000;
+
+                    // TcpListener server = new TcpListener(port);
+                    server = new TcpListener(IPAddress.Any, port);
+
+                    // Start listening for client requests.
+                    server.Start();
+
+                    // Buffer for reading data
+                    Byte[] bytes = new Byte[512];
+                    String data = null;
+
+                    // Enter the listening loop. 
+                    while (true)
+                    {
+                        Console.Write("Waiting for a connection... ");
+
+                        // Perform a blocking call to accept requests. 
+                        // You could also user server.AcceptSocket() here.
+                        TcpClient client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
+
+                        data = null;
+
+                        // Get a stream object for reading and writing
+                        NetworkStream stream = client.GetStream();
+
+                        int i;
+
+                        // Loop to receive all the data sent by the client. 
+                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            // Translate data bytes to a ASCII string.
+                            //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                            Console.WriteLine("Data received");
+                            //Console.WriteLine("Received: {0}", data);
+
+                            // Process the data sent by the client.
+                            //data = data.ToUpper();
+
+                            var m = (Message)DeserializeFromStream(new MemoryStream(bytes));
+
+                            m = MessageEnhacer.Enhance(m);
+
+                            byte[] msg = SerializeToStream(m).ToArray();
+
+                            //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                            // Send back a response.
+                            stream.Write(msg, 0, msg.Length);
+                            Console.WriteLine("Response sent");
+                            //Console.WriteLine("Sent: {0}", data);
+                        }
+
+                        // Shutdown and end connection
+                        client.Close();
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+                finally
+                {
+                    // Stop listening for new clients.
+                    server.Stop();
+                }
+
+
+                Console.WriteLine("\nHit enter to continue...");
+                Console.Read();
+            }  
+        }
+
+        /// <summary>
+        /// serializes the given object into memory stream
+        /// </summary>
+        /// <param name="objectType">the object to be serialized</param>
+        /// <returns>The serialized object as memory stream</returns>
+        public static MemoryStream SerializeToStream(object objectType)
+        {
+            MemoryStream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, objectType);
+            return stream;
+        }
+
+        /// <summary>
+        /// deserializes as an object
+        /// </summary>
+        /// <param name="stream">the stream to deserialize</param>
+        /// <returns>the deserialized object</returns>
+        public static object DeserializeFromStream(MemoryStream stream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            stream.Seek(0, SeekOrigin.Begin);
+            object objectType = formatter.Deserialize(stream);
+            return objectType;
+        }
+    }
+}
